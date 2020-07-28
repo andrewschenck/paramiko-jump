4aacb193b193e28150efd9473643d5fbe7c3e3a0
tybruno <tybruno@cisco.com>
2020-07-28 14:08:15 -0600

    added the ability to programatically authenticate with duo

--- paramiko_jump/client.py
+++ paramiko_jump/client.py
@@ -55,6 +55,71 @@ def simple_auth_handler(
         answers.append(input_(prompt))
     return answers
 
+def duo_handler(
+        title: AnyStr,
+        instructions: AnyStr,
+        prompt_list: Sequence[Prompt],
+) -> List[AnyStr]:
+    """
+    The purpose of this handler is to automate auth handlers that use duo. This removes prompting the user to
+    enter password and duo auth mode so that auth can be handled programmatically without user input at run time.
+
+    Authentication callback, for keyboard-interactive
+    authentication that uses DUO Auth.
+
+    Example:
+        # set the duo_handler password that will be entered to console
+        duo_handler.password = password
+        with SSHJumpClient(auth_handler=duo_handler) as jumper:
+            jumper.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            jumper.connect(
+                hostname=jump_host,
+                username=jump_user,
+            )
+            # Now I instantiate a session for the Jump Host <-> Target
+            # Host connection, and inject the jump_session to use for
+            # proxying.
+            target = SSHJumpClient(jump_session=jumper)
+            target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            target.connect(
+                hostname=target_host,
+                username=username,
+                password=password,
+                look_for_keys=False,
+                allow_agent=False,
+            )
+            _, stdout, _ = target.exec_command('sh ip int br')
+            print(stdout.read().decode())
+            target.close()
+
+    :param title:
+        Displayed to the end user before anything else.
+    :param instructions:
+        Displayed to the end user. Typically contains text explaining
+        the authentication scheme and / or legal disclaimers.
+    :param prompt_list:
+        A Sequence of (AnyStr, bool). Each string element is
+        displayed as an end-user input prompt. The corresponding
+        boolean element indicates whether the user input should
+        be 'echoed' back to the terminal during the interaction.
+    """
+    answers = []
+    if title:
+        print(title)
+    if instructions:
+        print(instructions)
+    for prompt, show_input in prompt_list:
+
+        if 'password' in prompt.lower() and duo_handler.password:
+            input_ = duo_handler.password
+            answers.append(input_)
+        elif 'duo' in prompt.lower():
+            print("Approve Duo Mobile Login Request On Your Device ")
+            answers.append("1")
+        else:
+            input_ = input if show_input else getpass
+            answers.append(input_(prompt))
+    return answers
 
 class SSHJumpClient(SSHClient):
     """
