5bad65bf4fd4ffac28dad7f4dbd68eea9a89f402
Andrew Schenck <aschenck@gmail.com>
2024-08-19 13:45:14 -0700

    Adding MultiFactorAuthHandler class (#37)
    - New handlerclass: MultiFactorAuthHandler, to depercate MagicAuthHandler
     - Tests for MultiFactorAuthHandler
     - Improved tests for the rest of the code (now 100% code coverage)
     - Updated README.md
    
    Co-authored-by: Andrew Schenck <aschenck@cisco.com>

--- paramiko_jump/handler.py
+++ paramiko_jump/handler.py
@@ -10,28 +10,34 @@ This module provides two authentication handlers:
     - simple_auth_handler, for simple keyboard-interactive use-cases
     - MagicAuthHandler, for mimicking keyboard-interactive authentication
       using pre-defined responses.
-
-
 """
-
-
-from getpass import getpass
-from typing import List, Sequence, Tuple
 
 
 __all__ = (
     'MagicAuthHandler',
+    'MultiFactorAuthHandler',
     'simple_auth_handler',
 )
 
 
-_Prompt = Tuple[str, bool]
+import logging
+
+from getpass import getpass
+from typing import (
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+)
+
+
+_LOG = logging.getLogger(__name__)
 
 
 def simple_auth_handler(
         title: str,
         instructions: str,
-        prompt_list: Sequence[_Prompt],
+        prompt_list: Sequence[Tuple[str, bool]],
 ) -> List[str]:
     """
     Authentication callback, for keyboard-interactive
@@ -50,9 +56,9 @@ def simple_auth_handler(
     """
     answers = []
     if title:
-        print(title)
+        _LOG.info(title)
     if instructions:
-        print(instructions)
+        _LOG.info(instructions)
 
     for prompt, show_input in prompt_list:
         input_ = input if show_input else getpass
@@ -62,10 +68,10 @@ def simple_auth_handler(
 
 class MagicAuthHandler:
     """
-    Stateful auth handler for paramiko that will return a list of
-    auth parameters for every CLI prompt. This is useful for multi-
-    factor authentication where the auth parameters change with each
-    prompt (e.g. password followed by OTP).
+    Stateful auth handler for paramiko that will return an auth parameter
+    for each CLI prompt. This is useful for multi-factor authentication
+    where the auth parameters change with each prompt (e.g. password
+    followed by OTP).
 
     Example
     -------
@@ -77,9 +83,16 @@ class MagicAuthHandler:
         ['1234']
 
 
+    This class is deprecated by the ``MultiFactorAuthHandler``; use that
+    class instead for new use cases.
     """
 
     def __init__(self, *items):
+        """
+        :param items:
+            Auth responses to be returned by the handler. Each should be a
+            List.
+        """
         self._iterator = iter(items)
 
     def __call__(self, *args, **kwargs):
@@ -93,3 +106,100 @@ class MagicAuthHandler:
 
     def __next__(self):
         return next(self._iterator)
+
+
+class MultiFactorAuthHandler:
+    """
+    Stateful auth handler for paramiko that will return an auth parameter
+    for each CLI prompt. This is useful for multi-factor authentication
+    where the auth parameters change with each prompt (e.g. password
+    followed by OTP).
+
+    This class is a more flexible and feature-rich replacement for the
+    deprecated ``MagicAuthHandler``.
+
+    Example
+    -------
+        >>> from paramiko_jump import MultiFactorAuthHandler
+        >>> handler = MultiFactorAuthHandler()
+        >>> handler.add('password')
+        >>> handler.add('1234')
+        >>> handler()
+        ['password']
+        >>> handler()
+        ['1234']
+
+    Backwards-compatibility Example
+    -------------------------------
+        >>> from paramiko_jump import MultiFactorAuthHandler
+        >>> handler = MultiFactorAuthHandler(['password'], ['1234'])
+        >>> handler()
+        ['password']
+        >>> handler()
+        ['1234']
+    """
+
+    def __init__(
+            self,
+            *auth_responses: List,
+            show_title: bool = False,
+            show_instructions: bool = False,
+            show_prompts: bool = False,
+
+    ):
+        """
+        :param auth_responses:
+            Optional auth responses to be returned by the handler.
+            Each item should be a List. This interface is backwards-
+            compatible with the ``MagicAuthHandler``
+
+            Alternatively, see the .add() method for an easier
+            approach to adding auth responses after instantiation,
+            without needing to pack them into Lists.
+        :param show_title:
+            If True, the title will be displayed.
+        :param show_instructions:
+            If True, the instructions will be displayed.
+        :param show_prompts:
+            If True, the authentication prompts will be displayed.
+        """
+        self._auth_responses = [*auth_responses]
+        self._show_title = show_title
+        self._show_instructions = show_instructions
+        self._show_prompts = show_prompts
+
+        self._iterator = None
+
+    def __call__(
+            self,
+            title: Optional[str] = None,
+            instructions: Optional[str] = None,
+            prompt_list: Optional[List] = None,
+    ):
+        if self._show_title:
+            _LOG.info(title)
+        if self._show_instructions:
+            _LOG.info(instructions)
+        if self._show_prompts and prompt_list:
+            for prompt, _ in prompt_list:
+                _LOG.info(prompt)
+        try:
+            return next(self)
+        except StopIteration:
+            return []
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._iterator is None:
+            self._iterator = iter(self._auth_responses)
+
+        return next(self._iterator)
+
+    def add(self, auth_response: str) -> None:
+        """
+        Add an auth response to the handler.
+        """
+        # Pack it into a list as this is how Paramiko expects it.
+        self._auth_responses.append([auth_response])
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,6 +1,10 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from paramiko_jump.handler import simple_auth_handler, MagicAuthHandler
+from unittest.mock import call, patch
+from paramiko_jump.handler import (
+    MagicAuthHandler,
+    MultiFactorAuthHandler,
+    simple_auth_handler,
+)
 
 
 class TestSimpleAuthHandler(unittest.TestCase):
@@ -35,6 +39,7 @@ class TestSimpleAuthHandler(unittest.TestCase):
         mock_input.assert_called_once_with("Enter username: ")
         mock_getpass.assert_called_once_with("Enter password: ")
 
+
 class TestMagicAuthHandler(unittest.TestCase):
 
     def test_magic_auth_handler(self):
@@ -50,6 +55,53 @@ class TestMagicAuthHandler(unittest.TestCase):
         self.assertEqual(next(iterator), ['1234'])
         with self.assertRaises(StopIteration):
             next(iterator)
+
+
+class TestMultiFactorAuthHandler(unittest.TestCase):
+
+    def test_init_with_auth_responses(self):
+        handler = MultiFactorAuthHandler(['password'], ['1234'])
+        self.assertEqual(handler._auth_responses, [['password'], ['1234']])
+
+    def test_init_without_auth_responses(self):
+        handler = MultiFactorAuthHandler()
+        self.assertEqual(handler._auth_responses, [])
+
+    def test_call_with_auth_responses(self):
+        handler = MultiFactorAuthHandler(['password'], ['1234'])
+        self.assertEqual(handler(), ['password'])
+        self.assertEqual(handler(), ['1234'])
+        self.assertEqual(handler(), [])
+
+    def test_call_with_title_instructions_prompts(self):
+        handler = MultiFactorAuthHandler(show_title=True, show_instructions=True, show_prompts=True)
+        with patch('paramiko_jump.handler._LOG.info') as mock_log_info:
+            handler('Title', 'Instructions', [('Prompt', True)])
+            mock_log_info.assert_has_calls([
+                call('Title'),
+                call('Instructions'),
+                call('Prompt')
+            ])
+
+    def test_add_auth_response(self):
+        handler = MultiFactorAuthHandler()
+        handler.add('password')
+        self.assertEqual(handler._auth_responses, [['password']])
+
+    def test_iter(self):
+        handler = MultiFactorAuthHandler(['password'], ['1234'])
+        iterator = iter(handler)
+        self.assertEqual(next(iterator), ['password'])
+        self.assertEqual(next(iterator), ['1234'])
+        with self.assertRaises(StopIteration):
+            next(iterator)
+
+    def test_next(self):
+        handler = MultiFactorAuthHandler(['password'], ['1234'])
+        self.assertEqual(next(handler), ['password'])
+        self.assertEqual(next(handler), ['1234'])
+        with self.assertRaises(StopIteration):
+            next(handler)
 
 
 if __name__ == '__main__':
